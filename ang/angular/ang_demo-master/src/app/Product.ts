a8ac6abd4aa5bfbd34ed4d50ba092019a0ee9a6d
jyotiprodapt <jyoti.r@prodapt.com>
2022-07-19 09:38:28 +0530

    initial

--- ang/angular/ang_demo-master/src/app/Product.ts
+++
@@ -1,5 +0,0 @@
-export interface Product{
-    name: string;
-    brand: string;
-    price: number;
-}
--- angularbasics/Employee_app_angular-master/src/app/Employees.ts
+++
@@ -1,6 +0,0 @@
-export interface Employee{
-    id ?: 1;
-    name : string;
-    profession : string;
-    project : boolean;
-}
